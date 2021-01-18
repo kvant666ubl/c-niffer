8b3e0dd7705677f7a23c05fcf14d6f3fd18aa7c4
kvant666ubl <61603113+kvant666ubl@users.noreply.github.com>
2021-01-18 19:55:57 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,28 +1,27 @@
-#include<getopt.h>  // for cli
-#include<pcap.h>    // lib fo sniff
+#include<pcap.h>    // network analysis
 
-#include <stdio.h>	// for in/out
-#include <stdlib.h> // for exit()
-#include <string.h> // for memset
-#include <getopt.h> // fot cli
-#include <unistd.h> // daem
+#include <stdio.h>  // in/out
+#include <stdlib.h> // exit()
+#include <string.h> // memset
+#include <getopt.h> // CLI
+#include <unistd.h> 
 
-#include <syslog.h>
-#include <sys/types.h>
+#include <syslog.h>        // logging to /var/log/syslog
+#include <sys/types.h>  
 #include <sys/stat.h>
 #include <sys/socket.h>    // socket lib for commun
 #include <stdlib.h>
 
-#include <arpa/inet.h>     // for inet_ntoa()
-#include <net/ethernet.h>  // for eth0 debice
+#include <arpa/inet.h>     // inet_ntoa()
+#include <net/ethernet.h>  // eth0 device
 
-#include <netinet/udp.h>	  // udp header
-#include <netinet/tcp.h>	  // tcp header
+#include <netinet/udp.h>   // udp header
+#include <netinet/tcp.h>   // tcp header
 
 #include <fcntl.h>
-#include <errno.h>
+#include <errno.h> // errors
 
-#include "func.h"
+#include "func.h" // funstions and constants 
 
 struct sockaddr_in source, dest;
 int tcp=0, udp=0, others=0, total=0, i, j; 
@@ -30,11 +29,10 @@ int tcp=0, udp=0, others=0, total=0, i, j;
 static int verbose_flag;
 int packets = 0;
 
-
-int main (int argc, char **argv) 
-{        
-    /* PCAP-part*/
-    pcap_if_t *alldevsp , *device;     
+int main (int argc, char **argv)  
+{
+	/* PCAP-part*/
+	pcap_if_t *alldevsp , *device;
 	pcap_t *handle; 				  
 	char errbuf[100] , *devname , devs[100][100];
 	int count = 1, n;
@@ -57,23 +55,24 @@ int main (int argc, char **argv)
 	int c;
 	uint8_t action = 0;
     
-
     /* Our process ID and Session ID */
     pid_t pid, sid; 
-    /* Fork off the parent process */
+    
+	/* Fork off the parent process */
     pid = fork();
     printf("PID is: %d\n", pid);
     if (pid < 0) {
-       exit(EXIT_FAILURE);
+    	exit(EXIT_FAILURE);
     }   
-
+    
     /* Parent Process */
     if (pid > 0) {
-       exit(EXIT_SUCCESS);
+    	exit(EXIT_SUCCESS);
     }   
-    /* Change the file mode mask */
+    
+	/* Change the file mode mask */
     umask(0);   
-
+    
     /* Writing in /var/log/syslog */   
     setlogmask (LOG_UPTO (LOG_NOTICE));
     openlog ("[C-NIFFER]", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1); 
@@ -81,112 +80,114 @@ int main (int argc, char **argv)
     /* Create a new SID for the child process */
     sid = setsid();
     if (sid < 0) {
-        /* Log the failure */
-        exit(EXIT_FAILURE);
-    }   
+    	/* Log the failure */
+    	exit(EXIT_FAILURE);
+    }
 
     /* Change the current working directory */
     if ((chdir("/")) < 0) {
-        /* Log the failure */
-        exit(EXIT_FAILURE);
+    	/* Log the failure */
+    	exit(EXIT_FAILURE);
     }
     
     /* The Big Loop */
-    while (1) {
-        while (1) {
+    while (1) 
+    {
+    	while (1) 
+        {
             static struct option long_options[] =
-            	{
-                  {"verbose", no_argument		, &verbose_flag, 1},
-                  {"brief",   no_argument		, &verbose_flag, 0},
-                  {"start"  , no_argument       , 0, 's'}, 			// s-start
-                  {"stop"   , no_argument       , 0, 'p'}, 			// p-pause
-                  {"show"   , required_argument , 0, 'v'}, 			// v-view
-                  {"select" , required_argument , 0, 'c'}, 			// c-choose 
-                  {"stat"   , required_argument , 0, 'i'}, 			// i-info 
-                  {"help"   , no_argument       , 0, 'h'}, 			// h-help
-            	  {"mode"   , required_argument , 0, 'm'},          // m-mo	de
-                  {0		, 0					, 0	  ,   0}  		// NULL
-                };
-        
-            int option_index = 0;
+        	{
+            	{"verbose", no_argument		, &verbose_flag, 1},
+            	{"brief",   no_argument		, &verbose_flag, 0},
+            	{"start"  , no_argument       , 0, 's'}, 			// s-start
+            	{"stop"   , no_argument       , 0, 'p'}, 			// p-pause
+            	{"show"   , required_argument , 0, 'v'}, 			// v-view
+            	{"select" , required_argument , 0, 'c'}, 			// c-choose 
+            	{"stat"   , required_argument , 0, 'i'}, 			// i-info 
+            	{"help"   , no_argument       , 0, 'h'}, 			// h-help
+        	{"mode"   , required_argument , 0, 'm'},          // m-mo	de
+            	{0		, 0					, 0	  ,   0}  		// NULL
+            	};
+    
+	    int option_index = 0;
             c = getopt_long (argc, argv, "sphv:c:i:m:",
-                    long_options, &option_index);
-
+                	long_options, &option_index);
+		    
             if (c == -1) break;
-
+            
             switch (c)
             {
-            case 0:
-                if (long_options[option_index].flag != 0)
-                    break;
-                printf ("option %s", long_options[option_index].name);
-                if (optarg) printf (" with arg %s", optarg);
-                printf ("\n");
-                break;
+        	    case 0:
+        	    	if (long_options[option_index].flag != 0) break;
+        	    	printf ("option %s", long_options[option_index].name);
+        	    	if (optarg) printf (" with arg %s", optarg);
+        	    	printf ("\n");
+        	    	break;
 
-            case 's':
-                devname = devs[4];
-                printf("[SNIFFING FROM DEFAULT %s INTERFACE] \n" , devname);
-                handle = pcap_open_live(devname , 65536 , 1 , 0  , errbuf);
-                if (handle == NULL)  
-                {
-                    syslog(LOG_NOTICE, "[CNIFFER] | [ERROR] Couldn't open device %s : %s\n" , devname , errbuf);
-                    exit(1);
-                }
-                action = 1;
-                break;
+        	    case 's':
+        	    	devname = devs[4];
+        	    	printf("[SNIFFING FROM DEFAULT %s INTERFACE] \n" , devname);
+        	    	handle = pcap_open_live(devname , 65536 , 1 , 0  , errbuf);
+        	    	if (handle == NULL)  
+        	    	{
+        	    			syslog(LOG_NOTICE, "[CNIFFER] | [ERROR] Couldn't open device %s : %s\n" , devname , errbuf);
+        	    			exit(1);
+        	    	}
+        	    	action = 1;
+        	    	break;
 
-            case 'p':
-                syslog(LOG_NOTICE, "[CNIFFER] | [SOON] Packets are not sniffed from now.");
-                break;
+        	    case 'p':
+        	    	syslog(LOG_NOTICE, "[CNIFFER] | [SOON] Packets are not sniffed from now.");
+        	    	break;
 
-            case 'h':
-                reference();
-                printf("%s[MY AVAILABLE DEVICES]\n", KYEL); printf("%s", KNRM);
-                for(device = alldevsp ; device != NULL ; device = device->next) {
-                    printf("%d. %s - %s\n" , count , device->name , device->description);
-                    if(device->name != NULL)
-                    {
-                        strcpy(devs[count] , device->name);
-                    }
-                    count++;
-                }
-                action = 0;
-                break;
+        	    case 'h':
+        	    	reference();
+        	    	printf("%s[MY AVAILABLE DEVICES]\n", KYEL); printf("%s", KNRM);
 
-            case 'v':
-                printf("[SOON] Print number of packets received from ip address");
-                break;
+			    	for(device = alldevsp ; device != NULL ; device = device->next) {
+                    				printf("%d. %s - %s\n" , count , device->name , device->description);
+                    				if(device->name != NULL)
+                    				{
+                        				strcpy(devs[count] , device->name);
+                    				}
+                    				count++;
+                	}
+                	action = 0;
+                	break;
 
-            case 'c':
-                devname = devs[atoi(optarg)];
-                printf("[SNIFFING FROM  %s INTERFACE] \n" , devname);
-                handle = pcap_open_live(devname , 65536 , 1 , 0 , errbuf); // for a 64bit Linux: 65535 bytes + 1
+                case 'v':
+                	printf("[SOON] Print number of packets received from ip address");
+                	break;
 
-                if (handle == NULL)  
-                {
-                    fprintf(stderr, "Couldn't open device %s : %s\n" , devname , errbuf);
-                    exit(1);
-                }
-                action = 1;
-                break;
+                case 'c':
+                	devname = devs[atoi(optarg)];
+                	printf("[SNIFFING FROM  %s INTERFACE] \n" , devname);
+                	handle = pcap_open_live(devname , 65536 , 1 , 0 , errbuf); // for a 64bit Linux: 65535 bytes + 1
+                	if (handle == NULL)  
+                	{
+                    		fprintf(stderr, "Couldn't open device %s : %s\n" , devname , errbuf);
+                    		exit(1);
+                	}
+                	action = 1;
+                	break;
 
-            case 'i':
-                printf("[SOON] Collected statistics for particular interface.");
-                break;
+                case 'i':
+                	printf("[SOON] Collected statistics for particular interface.");
+                	break;
 
-            case 'm':
-                break;
-                /*
-                // Fixing...
-                if (*optarg == 'P') 
-                    packets = 1;
-                */
-                break;
+                case 'm':
+                	/*
+                	// Fixing...
+                	if (*optarg == 'P') 
+                    	packets = 1;
+                	*/
+                	break;
 
-            case '?': break;
+                case '?': 
+			    	break;
 
-            default: abort ();
+                default: 
+			    	abort ();
             }
         }
 
@@ -196,29 +197,31 @@ int main (int argc, char **argv)
             while (optind < argc) printf ("%s ", argv[optind++]);
             putchar ('\n');
         }
-
-        if (action == 1) {  
         
-            pcap_set_timeout(handle, 1000);
-            pcap_loop(handle , -1 , process_packet , NULL);
+        if (action == 1) 
+	    {
+    	    pcap_set_timeout(handle, 1000);
+    	    pcap_loop(handle , -1 , process_packet , NULL);
         }
-        else
-        {
-            exit(EXIT_SUCCESS);
-        }
+    
+        else exit(EXIT_SUCCESS);
     }
+
     closelog();
     exit(EXIT_SUCCESS);
 }
 
+
+
 void process_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *buffer)
 {
-    int size = header->len;
-    struct iphdr *iph = (struct iphdr*)(buffer + sizeof(struct ethhdr));
-    ++total;
-    switch (iph->protocol)
-    {   
-        case 6:     // TCP 
+	int size = header->len;
+	struct iphdr *iph = (struct iphdr*)(buffer + sizeof(struct ethhdr));
+	++total;
+
+	switch (iph->protocol)
+	{   
+		case 6:     // TCP 
             ++tcp;
             print_tcp_packet(buffer , size);
             break;
@@ -233,9 +236,12 @@ void process_packet(u_char *args, const struct pcap_pkthdr *header, const u_char
             ++others;
             break;
     }
+
     if (packets == 1)
     printf("TCP : %d   UDP : %d   Others : %d   Total : %d\r", tcp , udp , others , total);
 }
+
+
 
 void print_ethernet_header(const u_char *Buffer, int Size)
 {
@@ -247,17 +253,17 @@ void print_ethernet_header(const u_char *Buffer, int Size)
     syslog(LOG_NOTICE, "|-Protocol            : %u \n",(unsigned short)eth->h_proto);
 }
 
+
+
 void print_ip_header(const u_char * Buffer, int Size)
 {
     print_ethernet_header(Buffer , Size);
     unsigned short iphdrlen;
         
     struct iphdr *iph = (struct iphdr *)(Buffer  + sizeof(struct ethhdr) );
-    iphdrlen = iph->ihl*4;
-    
+    iphdrlen = iph->ihl*4;    
     memset(&source, 0, sizeof(source));
     source.sin_addr.s_addr = iph->saddr;
-    
     memset(&dest, 0, sizeof(dest));
     dest.sin_addr.s_addr = iph->daddr;
     
@@ -275,6 +281,8 @@ void print_ip_header(const u_char * Buffer, int Size)
     syslog(LOG_NOTICE , "|-Destination IP   : %s\n" , inet_ntoa(dest.sin_addr) );
 }
 
+
+
 void print_tcp_packet(const u_char * Buffer, int Size)
 {
     unsigned short iphdrlen;
@@ -285,10 +293,8 @@ void print_tcp_packet(const u_char * Buffer, int Size)
     struct tcphdr *tcph=(struct tcphdr*)(Buffer + iphdrlen + sizeof(struct ethhdr));
     int header_size =  sizeof(struct ethhdr) + iphdrlen + tcph->doff*4;
     
-    syslog(LOG_NOTICE , "\n\n***********************TCP Packet*************************\n"); 
-            
+    syslog(LOG_NOTICE , "\n\n***********************TCP Packet*************************\n");             
     print_ip_header(Buffer,Size);
-        
     syslog(LOG_NOTICE , "\n");
     syslog(LOG_NOTICE , "TCP Header\n");
     syslog(LOG_NOTICE , "|-Source Port          : %u\n",ntohs(tcph->source));
@@ -308,18 +314,15 @@ void print_tcp_packet(const u_char * Buffer, int Size)
     syslog(LOG_NOTICE , "\n");
     syslog(LOG_NOTICE , "                        HEX-DATA DUMP                     ");
     syslog(LOG_NOTICE , "\n");
-        
     syslog(LOG_NOTICE, "IP Header\n");
     print_data(Buffer,iphdrlen);
-    
     syslog(LOG_NOTICE, "TCP Header\n");
     print_data(Buffer+iphdrlen,tcph->doff*4);
-        
     syslog(LOG_NOTICE, "Data Payload\n");
     print_data(Buffer + header_size , Size - header_size );
-                        
     syslog(LOG_NOTICE, "\n###########################################################");
 }
+
 
 
 void print_udp_packet(const u_char *Buffer , int Size)
@@ -328,14 +331,11 @@ void print_udp_packet(const u_char *Buffer , int Size)
     struct iphdr *iph = (struct iphdr *)(Buffer +  sizeof(struct ethhdr));
     iphdrlen = iph->ihl*4;
     
-    struct udphdr *udph = (struct udphdr*)(Buffer + iphdrlen  + sizeof(struct ethhdr));
-    
+    struct udphdr *udph = (struct udphdr*)(Buffer + iphdrlen  + sizeof(struct ethhdr));    
     int header_size =  sizeof(struct ethhdr) + iphdrlen + sizeof udph;
     
     syslog(LOG_NOTICE, "\n\n***********************UDP Packet*************************\n");
-    
     print_ip_header(Buffer,Size);           
-    
     syslog(LOG_NOTICE, "\nUDP Header\n");
     syslog(LOG_NOTICE,  "|-Source Port      : %d\n" , ntohs(udph->source));
     syslog(LOG_NOTICE,  "|-Destination Port : %d\n" , ntohs(udph->dest));
@@ -345,28 +345,26 @@ void print_udp_packet(const u_char *Buffer , int Size)
     syslog(LOG_NOTICE,  "\n");
     syslog(LOG_NOTICE, "IP Header\n");
     print_data(Buffer , iphdrlen);
-        
     syslog(LOG_NOTICE,  "UDP Header\n");
     print_data(Buffer+iphdrlen , sizeof udph);
-        
     syslog(LOG_NOTICE, "Data Payload\n");    
     print_data(Buffer + header_size , Size - header_size);
-    
     syslog(LOG_NOTICE, "\n###########################################################");
 }
+
 
 
 void print_data (const u_char * data , int Size)
 {
     int i , j;
-    for(i=0 ; i < Size ; i++)
+    for(i = 0 ; i < Size ; i++)
     {
-        if( i!=0 && i%16==0)   
+        if( i != 0 && i % 16 == 0)   
         {
             syslog(LOG_NOTICE, "         ");
-            for(j=i-16 ; j<i ; j++)
+            for(j = i-16; j < i; j++)
             {
-                if(data[j]>=32 && data[j]<=128) syslog(LOG_NOTICE, "%c",(unsigned char)data[j]);
+                if(data[j]>=32 && data[j]<=128) syslog(LOG_NOTICE, "%c", (unsigned char)data[j]);
                 else syslog(LOG_NOTICE, ".");
             }
             syslog(LOG_NOTICE, "\n");
@@ -375,7 +373,7 @@ void print_data (const u_char * data , int Size)
         if(i%16==0) syslog(LOG_NOTICE, "   ");
             syslog(LOG_NOTICE, " %02X", (unsigned int)data[i]);
                 
-        if( i==Size-1) 
+        if(i==Size-1) 
         {
             for(j=0;j<15-i%16;j++) 
             {
@@ -398,18 +396,19 @@ void print_data (const u_char * data , int Size)
     }
 }
 
+
+
 void reference() {
     printf("\n");
     printf("%s[USAGE INFORMATION]\n", KYEL);
     printf("%s➤ Examples: \n", KNRM);
-    printf("%-32s", "  sudo ./cniff --help"); 			printf("%s\n", " →  get usage information or your net-devices");
-    printf("%-32s", "  sudo ./cniff --start");			printf("%s\n", " →  sniff packets from default interface (eth0)");
+    printf("%-32s", "  sudo ./cniff --help"); 		printf("%s\n", " →  get usage information or your net-devices");
+    printf("%-32s", "  sudo ./cniff --start");		printf("%s\n", " →  sniff packets from default interface (eth0)");
     printf("%-32s", "  sudo ./cniff --select [iface]"); printf("%s\n", " →  sniff packets using wireless interface (dev-tab)");
     printf("%-32s", "  sudo ./cniff --stat [iface]");   printf("%s\n", " →  show collected statistics for particular interface");
     printf("%-32s", "  sudo ./cniff --show [ip]");      printf("%s\n", " →  print number of packets received from [ip] address");
     printf("%-32s", "  sudo ./cniff --stop");           printf("%s\n", " →  stop sniffing");
     printf("\n");
-
     printf("%s[DEVICES TABLE] (dev-tab)\n", KYEL); 
     printf("%s➤ When using a '--select' or '-c' option value of option must be:\n", KNRM);
     printf("	<1> Networking wireless interface (e.g. wlan0,  wlp2s0..)\n");
